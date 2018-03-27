cc11b2fcdb4e9499700d398f1c00f390a62977f5
Harry Pidcock <haza55@gmail.com>
2018-03-27 17:51:12 +1000

    Fixing access of transport method. gRPC internal method was removed, but added a better method to access the method :)

--- proxy/handler.go
+++ proxy/handler.go
@@ -9,7 +9,6 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/transport"
 )
 
 var (
@@ -59,12 +58,10 @@ type handler struct {
 // It is invoked like any gRPC server stream and uses the gRPC server framing to get and receive bytes from the wire,
 // forwarding it to a ClientStream established against the relevant ClientConn.
 func (s *handler) handler(srv interface{}, serverStream grpc.ServerStream) error {
-	// little bit of gRPC internals never hurt anyone
-	lowLevelServerStream, ok := transport.StreamFromContext(serverStream.Context())
+	fullMethodName, ok := grpc.MethodFromServerStream(serverStream) 
 	if !ok {
-		return grpc.Errorf(codes.Internal, "lowLevelServerStream not exists in context")
+		return grpc.Errorf(codes.Internal, "failed to get method from server stream")
 	}
-	fullMethodName := lowLevelServerStream.Method()
 	// We require that the director's returned context inherits from the serverStream.Context().
 	outgoingCtx, backendConn, err := s.director(serverStream.Context(), fullMethodName)
 	clientCtx, clientCancel := context.WithCancel(outgoingCtx)
