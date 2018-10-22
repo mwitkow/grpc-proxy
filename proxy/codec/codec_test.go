0cc29f9b90c8d75cdd64fdc8eb054f9a668ff84f
Ingmar Wittkau <iwittkau@users.noreply.github.com>
2018-10-22 09:17:42 +0200

    rewrite codec - put codec in subpackage - use encoding.Codec - add comments

---
+++ proxy/codec/codec_test.go
@@ -0,0 +1,57 @@
+package codec_test
+
+import (
+	"reflect"
+	"testing"
+
+	_ "github.com/gogo/protobuf/proto"
+	codec "github.com/mwitkow/grpc-proxy/proxy/codec"
+	pb "github.com/mwitkow/grpc-proxy/testservice"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/encoding"
+)
+
+// trigger init() of proxy/codec package befor tests run
+var _ = codec.Proxy{}
+
+func TestCodec_ReadYourWrites(t *testing.T) {
+	framePtr := &codec.Frame{}
+	data := []byte{0xDE, 0xAD, 0xBE, 0xEF}
+	encoding.RegisterCodec(codec.Proxy{})
+	codec := encoding.GetCodec(codec.Proxy{}.Name())
+	t.Log(reflect.TypeOf(codec).String())
+	require.NotNil(t, codec, "codec must be registered")
+	require.NoError(t, codec.Unmarshal(data, framePtr), "unmarshalling must go ok")
+	out, err := codec.Marshal(framePtr)
+	require.NoError(t, err, "no marshal error")
+	require.Equal(t, data, out, "output and data must be the same")
+
+	// reuse
+	require.NoError(t, codec.Unmarshal([]byte{0x55}, framePtr), "unmarshalling must go ok")
+	out, err = codec.Marshal(framePtr)
+	require.NoError(t, err, "no marshal error")
+	require.Equal(t, []byte{0x55}, out, "output and data must be the same")
+
+}
+
+func TestProtoCodec_ReadYourWrites(t *testing.T) {
+	p1 := &pb.PingRequest{
+		Value: "test-ping",
+	}
+	proxyCd := encoding.GetCodec(codec.Proxy{}.Name())
+
+	require.NotNil(t, proxyCd, "proxy codec must not be nil")
+
+	out1p1, err := proxyCd.Marshal(p1)
+	require.NoError(t, err, "marshalling must go ok")
+	out2p1, err := proxyCd.Marshal(p1)
+	require.NoError(t, err, "marshalling must go ok")
+
+	p2 := &pb.PingRequest{}
+	err = proxyCd.Unmarshal(out1p1, p2)
+	require.NoError(t, err, "unmarshalling must go ok")
+	err = proxyCd.Unmarshal(out2p1, p2)
+	require.NoError(t, err, "unmarshalling must go ok")
+
+	require.Equal(t, *p1, *p2)
+}
